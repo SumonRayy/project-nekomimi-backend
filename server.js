5a5c02facefaf88add8c0ef3b622cdfd29387e78
SumonRayy <sumonrayy@gmail.com>
2022-03-21 10:11:25 +0530

    🚂 express app initialized and listening to a PORT

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 6969;
+
+app.listen(PORT, () => {
+    console.log(`🚂...Server started on port ${PORT}!`);
+});
